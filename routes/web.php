796feb61e1ea43694ebf63f3577fd666a3effca2
AbuBkar-2000 <abdumannopovabubakir2000@mail.ru>
2023-01-05 23:45:56 +0500

    component folder created and done all small things

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index');
